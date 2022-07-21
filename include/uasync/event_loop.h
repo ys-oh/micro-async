6ed1e4f2387683c0b84e90d99baaeed99f1332d7
ys-oh <oyster90@naver.com>
2022-07-22 07:06:52 +0900

    add event_loop header

---
+++ include/uasync/event_loop.h
@@ -0,0 +1,29 @@
+#ifndef EVENT_LOOP_H_ 
+#define EVENT_LOOP_H_ 
+
+#include <stdint.h>
+#include <stdarg.h>
+#include <string.h>
+
+#include "uasync/callable.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define EVENT_LOOP_DEFAULT NULL
+
+#define EVENT_READ  1
+#define EVENT_WRITE 2
+
+extern void* event_loop_create();
+extern void event_loop_release(void* ctx);
+
+extern void event_loop_add_io_event(void* ctx, int fd, int event, callable_obj_base* callable);
+extern void event_loop_add_timed_event(void* ctx, uint64_t timeout, callable_obj_base* callable);
+extern void event_loop_run(void* ctx);
+
+#ifdef __cplusplus
+}
+#endif
+#endif // EVENT_LOOP_H_ 
